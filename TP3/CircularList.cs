dac6a1acbf106b1bea21a33f82eb0e8353a01834
Amaury Caballero <amcaballero2023@gmail.com>
2021-03-04 22:13:01 -0400

    Circular Linked list

---
+++ TP3/CircularList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP3
+{
+    public class Circularlist
+    {
+    public Node head, head1, head2; 
+  
+    public class Node  
+    { 
+        public int data; 
+        public Node next, prev; 
+  
+        public Node(int d) 
+        { 
+            data = d; 
+            next = prev = null; //next property is here
+        } 
+    } 
+    //rabbit = fast
+    //turtle or hare idk = slow
+    
+    public void splitList()  
+    { 
+        Node turtle = head; //even
+        Node rabbit = head; //odd
+  
+        if (head == null)  //is empty
+        { 
+            return; 
+        } 
+  
+        
+        while (rabbit.next != head && 
+               rabbit.next.next != head) //while they are not equal to head because its circular list
+                                         // whenever its equal to head just finish the cycle
+        { 
+            rabbit = rabbit.next.next; 
+            turtle = turtle.next; 
+        } 
+
+        if (rabbit.next.next == head) // even
+        { 
+            rabbit = rabbit.next; 
+        } 
+
+        head1 = head; 
+    
+        if (head.next != head) 
+        { 
+            head2 = turtle.next; 
+        } 
+          
+        rabbit.next = turtle.next; 
+  
+        turtle.next = head;
+ 
+    } 
+  
+    public void printList(Node node) 
+    { 
+        Node temp = node; 
+        if (node != null)  
+        { 
+            do
+            { 
+                Console.Write(temp.data + " "); 
+                temp = temp.next; 
+            } while (temp != node); 
+        } 
+    } 
+    
+    } 
+}
--- TP3/Program.cs
+++ TP3/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections;
+using System.Linq;
+using System.Text;
+using static TP3.Circularlist;
 namespace TP3
 {
     class Program
     {
         static void Main(string[] args)
         {
+        Circularlist list = new Circularlist(); 
+        Circularlist list2 = new Circularlist(); 
+  
+        // Teacher example: 5->2->1->3->7->4->0 odd
+        // First split: 5->2->1->3
+        // Second split result: 7->4->0
+
+        // Antoher example: 5->2->1->3->7->4 even
+        // First split: 5->2->1
+        // Second split: 3->7->4
+
+        list.head = new Node(5); 
+        list.head.next = new Node(2); 
+        list.head.next.next = new Node(1); 
+        list.head.next.next.next = new Node(3); 
+        list.head.next.next.next.next = new Node(7); 
+        list.head.next.next.next.next.next = new Node(4); 
+        list.head.next.next.next.next.next.next = new Node(0); 
+        list.head.next.next.next.next.next.next.next = list.head; 
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("\nOriginal Circular Linked list (odd) ");
+        list.printList(list.head);
+        // Split the list 
+        Console.ForegroundColor = ConsoleColor.Blue;
+        list.splitList(); 
+        Console.WriteLine("\nFirst Circular List "); 
+        list.printList(list.head1); 
+        Console.WriteLine("\nSecond Circular List "); 
+        list.printList(list.head2); 
+
+
+        /*EVEN*/
+        list2.head = new Node(5); 
+        list2.head.next = new Node(2); 
+        list2.head.next.next = new Node(1); 
+        list2.head.next.next.next = new Node(3); 
+        list2.head.next.next.next.next = new Node(7); 
+        list2.head.next.next.next.next.next = new Node(4); 
+        list2.head.next.next.next.next.next.next = list2.head; 
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("\n\nOriginal Circular Linked list (even) "); 
+        list2.printList(list2.head); 
+  
+        // Split the list 
+        Console.ForegroundColor = ConsoleColor.Blue;
+        list2.splitList(); 
+        Console.WriteLine("\nFirst Circular List "); 
+        list2.printList(list2.head1); 
+        Console.WriteLine("\nSecond Circular List "); 
+        list2.printList(list2.head2); 
 
         }
     }
